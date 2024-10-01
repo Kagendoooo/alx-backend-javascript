e3547955c83dfa3e6132ba029843781ffbdada54
Gendozzzz <sunflowermaina@gmail.com>
2024-10-01 03:14:24 +0300

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
+  const task = false; // use 'const' because the value should not change
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
+    let task = true; // let to limit the scope of the block
     let task2 = false;
   }
 
