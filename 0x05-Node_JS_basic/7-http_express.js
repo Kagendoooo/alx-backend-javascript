417700228c5f2d84660c92c96b5bdc27b10b7a21
Gendozzzz <sunflowermaina@gmail.com>
2024-11-30 04:23:15 +0300

    Create a more complex server using express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -4,21 +4,21 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  const dbFile = process.argv[2];
-  res.status(200).send('This is the list of our students\n');
-  countStudents(dbFile)
-    .then(() => res.end())
+  countStudents(process.argv[2])
+    .then(() => {
+      res.write('This is the list of our students\n');
+      res.end();
+    })
     .catch((error) => {
-      res.status(500).send(error.message);
+      res.write(`${error.message}\n`);
+      res.end();
     });
 });
 
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
-});
+app.listen(1245);
 
 module.exports = app;
