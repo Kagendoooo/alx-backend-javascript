d86d96ccd090968563127a2cdba409b8159877a3
Gendozzzz <sunflowermaina@gmail.com>
2024-11-30 04:31:39 +0300

    Create a more complex server using express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -4,21 +4,26 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+  res.status(200).send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  countStudents(process.argv[2])
-    .then(() => {
-      res.write('This is the list of our students\n');
-      res.end();
+  const dbFile = process.argv[2];
+
+  if (!dbFile) {
+    res.status(400).send('Database file missing');
+    return;
+  }
+
+  res.status(200).write('This is the list of our students\n');
+
+  countStudents(dbFile)
+    .then((data) => {
+      res.end(data);
     })
     .catch((error) => {
-      res.write(`${error.message}\n`);
-      res.end();
+      res.status(500).send(error.message);
     });
 });
 
 app.listen(1245);
-
-module.exports = app;
