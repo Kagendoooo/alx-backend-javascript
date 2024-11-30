97a68f4f6712c2985cbe4e0c474dc3a8a5abbb11
Gendozzzz <sunflowermaina@gmail.com>
2024-11-30 03:49:12 +0300

    Create a complex server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,33 +2,19 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-  const { url } = req;
-
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  if (url === '/') {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
-  } else if (url === '/students') {
-    const dbPath = process.argv[2];
-    if (!dbPath) {
-      res.end('Database not provided');
-      return;
-    }
-
-    countStudents(dbPath)
-      .then(() => {
-        res.end(
-          'This is the list of our students\n'
-          + 'Number of students: 10\n'
-          + 'Number of students in CS: 6. List: Johann, Arielle, Jonathan, Emmanuel, Guillaume, Katie\n'
-          + 'Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy\n',
-        );
-      })
-      .catch((err) => {
-        res.end(`Error: ${err.message}`);
+  } else if (req.url === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2])
+      .then(() => res.end())
+      .catch((error) => {
+        res.end(error.message);
       });
-  } else {
-    res.end('Not Found');
   }
 });
 
